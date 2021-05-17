79bf26f65fc253bce577fefc363df882bd89c9fc
Emily Ury <ury.emily@gmail.com>
2021-05-17 11:49:31 -0400

    replace qwraps2 approach with a data summary using dplyr

--- NCDA_data_summary.R
+++ NCDA_data_summary.R
@@ -4,42 +4,37 @@
 
 setwd("C:/Users/uryem/Desktop/DukeBioDrop_backup/Ch3_SNAP_Carbon/data")
 
-
-library(qwraps2)
 library(dplyr)
-options(qwraps2_markup = "markdown")
+
 
 dat <- read.csv("NCDA_soils_data.csv", head = T)
-
 names(dat) <- c("Site", "Plot", "Depth", "ID", "Moisture", "W_V_g_cm3", "pH", "BaseSat", "AC", "CEC", "Na", "P", "K",
                 "Ca", "Mg", "S", "Mn", "Cu", "Zn", "EC")
 subset <- subset(dat, select = c(Site, Plot, Depth, ID, Moisture, pH, BaseSat, AC, CEC, EC))
 
 
-mean_sd(dat$pH, denote_sd = "paren")
-mci <- mean_ci(dat$pH)
-print(mci, show_level = TRUE)
-median_iqr(dat$pH)
-
-ST <- list("pH" = 
-             list("mean (sd)" = ~qwraps2::mean_sd(pH)),
-           "Base Saturation (0-5)" =
-             list("mean (sd)" = ~qwraps2::mean_sd(BaseSat[which(Depth == "(0-5)")])),
-           "Base Saturation (5-10)" =
-             list("mean (sd)" = ~qwraps2::mean_sd(BaseSat[which(Depth == "(5-10)")])),
-)
-
-table <- summary_table(dat, ST, by = c("Plot", "Site"))
-table
+pH <- subset %>%
+  group_by(Site, Plot, Depth) %>%
+  summarise(mean =round(mean(pH), 1), 
+            se = round(sd(pH)/sqrt(5), 3))
+pH
 
 
+BaseSat <- subset %>%
+  group_by(Site, Plot, Depth) %>%
+  summarise(mean =round(mean(BaseSat), 1), 
+            se = round(sd(BaseSat)/sqrt(5), 1))
+BaseSat
 
 
-ST <- list("pH" = 
-             list("mean (sd)" = ~qwraps2::mean_sd(pH)),
-           "Base Saturation" =
-             list("mean (sd)" = ~qwraps2::mean_sd(BaseSat))
-)
+CEC <- subset %>%
+  group_by(Site, Plot, Depth) %>%
+  summarise(mean =round(mean(CEC), 1), 
+            se = round(sd(CEC)/sqrt(5), 3))
+CEC
 
-table <- summary_table(dat, ST, by = c("Depth", "Plot", "Site"))
-table
+EC <- subset %>%
+  group_by(Site, Plot, Depth) %>%
+  summarise(mean =round(mean(EC), 2), 
+            se = round(sd(EC)/sqrt(5), 3))
+EC
