0902dcdce9240a2d71610a32127e0baf4f142909
Emily Ury <ury.emily@gmail.com>
2021-07-28 10:20:38 -0400

    added LOI fig 5 fixed figure numbering

--- manuscript_figures.R
+++ manuscript_figures.R
@@ -29,7 +29,7 @@ date <- c(rep("Aug 2020", 12), rep("Jun 2019", 12), rep("May 2018", 12), rep("Ju
 date <- factor(date, levels = c("May 2018", "Jul 2018", "Jun 2019", "Aug 2020") )
 
 
-############ Figure 2 -- pH
+############ Figure 8 -- pH
 #####
 
 x$response <- x$pH
@@ -62,7 +62,7 @@ s.pH <- output
   ### FIGURE  points and error bars 
   labs <- c("Depth: 0-5 cm", "Depth: 5-10 cm")
   names(labs) <- c("(0-5)", "(5-10)")
-tiff(filename = "Fig2.tiff", height=2400, width=3200, units= "px", res=800, compression= "lzw")
+tiff(filename = "Fig8.tiff", height=2400, width=3200, units= "px", res=800, compression= "lzw")
   ggplot(output, aes(x = date, y = mean, group = interaction(Treatment, Site), color = Treatment, linetype = Site, shape = Site)) +
     geom_point() +
     geom_line() +
@@ -76,7 +76,47 @@ tiff(filename = "Fig2.tiff", height=2400, width=3200, units= "px", res=800, comp
     geom_errorbar(data = output, aes(ymin = mean - se, ymax = mean + se, x = date, width = 0)) 
 dev.off()
   
+############ Figure 5 -- LOI
+#####
 
-  #####
+# x$response <- x$LOI
+# output <- x[,.("mean" = mean(response), "se" = std.error(response)),
+#             by = c("Date", "Site", "Treatment", "Depth")]
+# output$variable <- rep("pH", 48)
+# output$mean <- round(output$mean, 2)
+# output$se <- round(output$se, 2)
+# output$Site <- as.factor(output$Site)
+# output$Treatment <- as.factor(output$Treatment)
+# output$date <- date
+# s.pH <- output
 
+### FIGURE  points and error bars 
+
+x$response <- x$LOI
+output <- x[,.("mean" = mean(response), "se" = std.error(response)),
+            by = c("Date", "Site", "Treatment", "Depth")]
+output$variable <- rep("LOI", 48)
+output$mean <- round(output$mean, 2)
+output$se <- round(output$se, 2)
+output$Site <- as.factor(output$Site)
+output$Treatment <- as.factor(output$Treatment)
+output$date <- date
+s.LOI <- output
+
+### FIGURE  points and error bars 
+labs <- c("Depth: 0-5 cm", "Depth: 5-10 cm")
+names(labs) <- c("(0-5)", "(5-10)")
+tiff(filename = "Fig5.tiff", height=2400, width=3200, units= "px", res=800, compression= "lzw")
+ggplot(output, aes(x = date, y = mean, group = interaction(Treatment, Site), color = Treatment, linetype = Site, shape = Site)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(Depth ~ . ,labeller = labeller(Depth = labs)) + 
+  scale_color_manual(values=c("#000000", "#db351f"), labels = c("Control", "Salt")) +  
+  scale_linetype_manual(values=c("dotted", "longdash", "solid"), labels = c("Dry", "Int.", "Wet"))+
+  scale_shape_manual(values=c(17, 16, 15), labels = c("Dry", "Int.", "Wet")) +
+  theme_bw() +
+  xlab(" ") +
+  ylab("LOI (%)") +
+  geom_errorbar(data = output, aes(ymin = mean - se, ymax = mean + se, x = date, width = 0)) 
+dev.off()
 
